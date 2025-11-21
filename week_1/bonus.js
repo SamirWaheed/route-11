c27db4b0b5713860b5142def490b1398632d4bf3
samir wahid <samir00waheed@gmail.com>
2025-11-22 00:00:57 +0200

    Add bonus.js

---
+++ week_1/bonus.js
@@ -0,0 +1,24 @@
+var createCounter = function(init) {
+    let input = init;  
+
+    let func = {
+        increment: function() {
+           console.log(++input) ;   
+        },
+        decrement: function() {
+            input = init
+            console.log(--input) ; 
+        },
+        reset: function() {
+            input = init
+           console.log(input) ;
+        }
+    }
+
+    return func;
+};
+
+const counter = createCounter(6);
+counter.increment()
+counter.decrement(); 
+counter.reset();    
